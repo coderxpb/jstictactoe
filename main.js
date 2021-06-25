bcedcc0e3ca6f0f559c253dd24e2cd15a432985b
coderxpb <borapankaj901@gmail.com>
2021-06-25 15:20:23 +0530

    Add onClick function

--- main.js
+++ main.js
@@ -26,10 +26,34 @@ const Player = () =>{
     return {turn}
 }
 
+p1 = Player();
+p2 = Player();
+
 let gameController = (()=>{
-    
-    let onClick = (e)=>{
-        console.log(e.target);
+
+    let onClickGB = (e)=>{
+        divIndex = e.target.getAttribute("data-ind");
+        console.log(divIndex);
+        if(p1.turn==5){
+            console.log("Draw");
+            //reset everything
+        }
+        else if(p1.turn==p2.turn){
+            if(gameBoard.gameBoardArr[divIndex]==0){
+                gameBoard.updateGameBoard(divIndex,1)
+                p1.turn += 1
+            }
+        }
+        else{
+            if (gameBoard.gameBoardArr[divIndex] == 0) {
+                gameBoard.updateGameBoard(divIndex, 2)
+                p2.turn += 1
+            }
+        }
     }
+
+    tttCells.forEach(element => {
+        element.addEventListener('click', onClickGB, false);
+    });
 })();
 
