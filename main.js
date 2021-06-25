d2115930945b422a6b54dd3a4d76beba5724caf5
coderxpb <borapankaj901@gmail.com>
2021-06-25 16:00:00 +0530

    Add check for win function

--- main.js
+++ main.js
@@ -34,22 +34,69 @@ let gameController = (()=>{
     let onClickGB = (e)=>{
         divIndex = e.target.getAttribute("data-ind");
         console.log(divIndex);
-        if(p1.turn==5){
-            console.log("Draw");
-            //reset everything
+        if(p1.turn==4 && p2.turn==4){
+            if (gameBoard.gameBoardArr[divIndex] == 0) {
+                gameBoard.updateGameBoard(divIndex, 1)
+                p1.turn += 1
+                if (checkForWin(1)) {
+                    console.log("P1 wins!")
+                    console.log(gameBoard.gameBoardArr)
+                }
+                else{
+                    console.log("Draw");
+                    //reset everything
+                }
+            }
         }
+
         else if(p1.turn==p2.turn){
             if(gameBoard.gameBoardArr[divIndex]==0){
                 gameBoard.updateGameBoard(divIndex,1)
                 p1.turn += 1
+                if(checkForWin(1)){
+                    console.log("P1 wins!")
+                    console.log(gameBoard.gameBoardArr)
+                    //reset everything
+                }
             }
         }
         else{
             if (gameBoard.gameBoardArr[divIndex] == 0) {
                 gameBoard.updateGameBoard(divIndex, 2)
                 p2.turn += 1
+                if (checkForWin(2)) {
+                    console.log("P2 wins!")
+                    //reset everything
+                }
             }
         }
+    }
+
+    const checkForWin = (pNo) =>{
+        for(let i=0;i<3;i++){
+            if (gameBoard.gameBoardArr[i]==pNo){
+                if (gameBoard.gameBoardArr[i + 3] == pNo && gameBoard.gameBoardArr[i + 6] == pNo){
+                    return true;
+                }
+            }
+        }
+        for (let i = 0; i < 7; i+=3) {
+            if (gameBoard.gameBoardArr[i] == pNo) {
+                if (gameBoard.gameBoardArr[i + 1] == pNo && gameBoard.gameBoardArr[i + 2] == pNo) {
+                    return true;
+                }
+            }
+        }
+        if(gameBoard.gameBoardArr[4]==pNo){
+            if (gameBoard.gameBoardArr[0] == pNo && gameBoard.gameBoardArr[8] == pNo) {
+                return true;
+            }
+            if (gameBoard.gameBoardArr[2] == pNo && gameBoard.gameBoardArr[6] == pNo) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     tttCells.forEach(element => {
