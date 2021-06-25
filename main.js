087d25e14a1658fca31c1125faa577f2e7fd828b
coderxpb <borapankaj901@gmail.com>
2021-06-25 15:02:11 +0530

    Add x and o images

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 tttContainer = document.querySelector("#ttt-body")
-console.log("hey")
+tttCells = Array.from(document.querySelectorAll(".ttt-cell"))
+//tttCells[0].style.backgroundImage = "url('')";
+let gameBoard = (()=>{
+    let boardImg = [" ","xmark.png","omark.png"]
+    let gameBoardArr = [0,0,0,0,0,0,0,0,0];
+
+    const Clear = () =>{
+        gameBoardArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+
+    const updateGameBoard = (ind,playerNo) =>{
+        gameBoardArr[ind] = playerNo;
+        tttCells[ind].style.
+        
+    }
+
+    return{
+        gameBoardArr
+    };
+})();
+
+const Player = () =>{
+    let turn = 0;
+    return {turn}
+}
+
+let gameController = (()=>{
+    let onClick = (e)=>{
+        console.log(e.target);
+    }
+})();
